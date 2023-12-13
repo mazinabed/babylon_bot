77ca8117239483d0b231901c0dbbaa22c02b5589
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-13 19:19:29 +0300

    adding more commands

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,43 @@ bot.command('servicing', servicing());
 bot.on('message', greeting());
 // bot.on('message', servicing());
 
+/ Command to get information about Website Creation
+bot.command('website', (ctx) => {
+  const websiteInfo = `
+    Babyloncenter offers professional website creation services.
+    Our team of experts will help you design and develop a customized website tailored to your needs.
+  `;
+  ctx.reply(websiteInfo);
+});
+
+// Command to get information about Hosting Services
+bot.command('hosting', (ctx) => {
+  const hostingInfo = `
+    Babyloncenter provides reliable hosting services to ensure your website is always accessible.
+    Our hosting plans include features such as high uptime, security, and excellent customer support.
+  `;
+  ctx.reply(hostingInfo);
+});
+
+// Command to get information about Instagram Ads
+bot.command('instagram', (ctx) => {
+  const adsInfo = `
+    Boost your online presence with Babyloncenter's Instagram Ads services.
+    We help create and manage effective ad campaigns to reach your target audience on Instagram.
+  `;
+  ctx.reply(adsInfo);
+});
+
+// Command to get information about Web Development Training
+bot.command('web', (ctx) => {
+  const trainingInfo = `
+    Enhance your web development skills with Babyloncenter's training programs.
+    Our training sessions cover various topics, from beginner to advanced levels, to help you succeed in web development.
+  `;
+  ctx.reply(trainingInfo);
+});
+
+
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
   await production(req, res, bot);
