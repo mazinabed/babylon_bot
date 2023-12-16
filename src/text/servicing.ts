e532ccc659848a88e18797962a6594d301062e0e
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-16 18:10:33 +0300

    fixing issues

--- src/text/servicing.ts
+++ src/text/servicing.ts
@@ -30,38 +30,66 @@
 
 // export { servicing };
 
-import { Context, Markup } from 'telegraf';
-import createDebug from 'debug';
+// import { Context, Markup } from 'telegraf';
+// import createDebug from 'debug';
+
+// const debug = createDebug('bot:greeting_text');
+
+// const replyToMessage = (ctx: Context, messageId: number, text: string) =>
+//   ctx.reply(text, {
+//     reply_to_message_id: messageId,
+//   });
+
+// const servicing = () => async (ctx: Context) => {
+//   debug('Triggered "service" text command');
+
+//   const messageId = ctx.message?.message_id;
+//   const userName = `${ctx.message?.from.first_name}`;
+
+//   if (messageId) {
+//     await replyToMessage(ctx, messageId, `
+//     يقدم مركز بابلون مجموعة من الخدمات التعليمية والتكنلوجية المتنوعةابرزها
+//   `);
+
+//   // Use inline keyboard for services
+//   await ctx.reply('Choose a service:', {
+//     reply_markup:Markup.inlineKeyboard([
+//       Markup.button.callback('Website Creation', 'website'),
+//       Markup.button.callback('Host Services', 'hosting'),
+//       Markup.button.callback('Social Marketing', 'instagram'),
+//       Markup.button.callback('Training Courses', 'web'),
+//       Markup.button.callback('Telegram Bots', 'telegram'),
+//     ]) as any,
+//   });
+//   }
+// };
+
+// export { servicing };
+const { Markup } = require('telegraf');
+const createDebug = require('debug');
 
 const debug = createDebug('bot:greeting_text');
 
-const replyToMessage = (ctx: Context, messageId: number, text: string) =>
+const replyToMessage = (ctx, messageId, text) => {
   ctx.reply(text, {
     reply_to_message_id: messageId,
   });
+};
 
-const servicing = () => async (ctx: Context) => {
-  debug('Triggered "service" text command');
-
+const servicing = () => async (ctx) => {
   const messageId = ctx.message?.message_id;
-  const userName = `${ctx.message?.from.first_name}`;
 
   if (messageId) {
-    await replyToMessage(ctx, messageId, `
-    يقدم مركز بابلون مجموعة من الخدمات التعليمية والتكنلوجية المتنوعةابرزها
-  `);
-
-  // Use inline keyboard for services
-  await ctx.reply('Choose a service:', {
-    reply_markup:Markup.inlineKeyboard([
-      Markup.button.callback('Website Creation', 'website'),
-      Markup.button.callback('Host Services', 'hosting'),
-      Markup.button.callback('Social Marketing', 'instagram'),
-      Markup.button.callback('Training Courses', 'web'),
-      Markup.button.callback('Telegram Bots', 'telegram'),
-    ]) as any,
-  });
+    await ctx.reply('Choose a service:', {
+      ...Markup.inlineKeyboard([
+        Markup.button.callback('Website Creation', 'website'),
+        Markup.button.callback('Host Services', 'hosting'),
+        Markup.button.callback('Social Marketing', 'instagram'),
+        Markup.button.callback('Training Courses', 'web'),
+        Markup.button.callback('Telegram Bots', 'telegram'),
+      ]),
+    });
   }
 };
 
-export { servicing };
+module.exports = { servicing };
