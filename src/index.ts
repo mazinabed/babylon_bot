f9cba326f22eea3b23dae0b2912f7fd4b737e479
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-16 19:04:34 +0300

    adding commands

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,19 @@ const ENVIRONMENT = process.env.NODE_ENV || '';
 const bot = new Telegraf(BOT_TOKEN);
 
 // Register the handleButtonClick function as a command handler
-bot.command('website', handleButtonClick());
+bot.command('Website', handleButtonClick());
 bot.command('hosting', handleButtonClick());
 bot.command('instagram', handleButtonClick());
 bot.command('web', handleButtonClick());
 bot.command('telegram', handleButtonClick());
 
-bot.command('about', about());
-bot.command('Services', servicing());
-bot.command('Website', website());
-bot.command('Host', host());
-bot.command('Social', Social());
-bot.command('Training', Trainging());
-bot.command('Telegram',Bots())
+// bot.command('about', about());
+// bot.command('Services', servicing());
+// bot.command('Website', website());
+// bot.command('Host', host());
+// bot.command('Social', Social());
+// bot.command('Training', Trainging());
+// bot.command('Telegram',Bots())
 bot.on('message', greeting());
 
 
