a994a05e3f3ce37c1ea3ec0af6bdc18fdfb91fc6
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-16 19:18:11 +0300

    adding more details

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Telegraf } from 'telegraf';
 
 import { about, Bots, host, Social, Trainging, website } from './commands';
-import { greeting, servicing,handleButtonClick } from './text';
+import { greeting, servicing } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 
@@ -10,20 +10,15 @@ const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const bot = new Telegraf(BOT_TOKEN);
 
-// Register the handleButtonClick function as a command handler
-bot.command('website', handleButtonClick());
-bot.command('hosting', handleButtonClick());
-bot.command('instagram', handleButtonClick());
-bot.command('web', handleButtonClick());
-bot.command('telegram', handleButtonClick());
 
-// bot.command('about', about());
+
+bot.command('about', about());
 bot.command('Services', servicing());
-// bot.command('Website', website());
-// bot.command('Host', host());
-// bot.command('Social', Social());
-// bot.command('Training', Trainging());
-// bot.command('Telegram',Bots())
+bot.command('Website', website());
+bot.command('Host', host());
+bot.command('Social', Social());
+bot.command('Training', Trainging());
+bot.command('Telegram',Bots())
 bot.on('message', greeting());
 
 
