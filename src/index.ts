26294721818f5e14581283552aef9d7ea20e7934
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-16 20:27:45 +0300

    adding more detaisls.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Telegraf } from 'telegraf';
 
 import { about, Bots, host, Social, Trainging, website } from './commands';
-import { greeting, servicing } from './text';
+import { greeting, servicing, handleButtonClick } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 
@@ -9,17 +9,26 @@ const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const bot = new Telegraf(BOT_TOKEN);
+// Register commands
+bot.command('website', website());
+bot.command('host', host());
+bot.command('social', Social());
+
+// Register text command
+bot.command('services', servicing());
+
+// Register button click handler
+bot.action(/.*/, handleButtonClick());
 
 
-
-bot.command('about', about());
-bot.command('Services', servicing());
-bot.command('Website', website());
-bot.command('Host', host());
-bot.command('Social', Social());
-bot.command('Training', Trainging());
-bot.command('Telegram',Bots())
-bot.on('message', greeting());
+// bot.command('about', about());
+// bot.command('Services', servicing());
+// bot.command('Website', website());
+// bot.command('Host', host());
+// bot.command('Social', Social());
+// bot.command('Training', Trainging());
+// bot.command('Telegram',Bots())
+// bot.on('message', greeting());
 
 
 
