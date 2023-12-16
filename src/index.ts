fb974b86b5598cf7bdb16aefe0d1113acf40ddc9
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-16 20:40:35 +0300

    adding detais to index

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,31 @@
-// import { Telegraf } from 'telegraf';
-
-// import { about, Bots, host, Social, Trainging, website } from './commands';
-// import { greeting, servicing } from './text';
-// import { VercelRequest, VercelResponse } from '@vercel/node';
-// import { development, production } from './core';
-
-// const BOT_TOKEN = process.env.BOT_TOKEN || '';
-// const ENVIRONMENT = process.env.NODE_ENV || '';
-
-// const bot = new Telegraf(BOT_TOKEN);
-
-// bot.command('about', about());
-// bot.command('Services', servicing());
-// bot.command('Website', website());
-// bot.command('Host', host());
-// bot.command('Social', Social());
-// bot.command('Training', Trainging());
-// bot.command('Telegram',Bots())
-// bot.on('message', greeting());
-
-
-
-
-
-// //prod mode (Vercel)
-// export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
-//   await production(req, res, bot);
-// };
-// //dev mode
-// ENVIRONMENT !== 'production' && development(bot);
 import { Telegraf } from 'telegraf';
-import { servicing, handleButtonClick } from './text';
-import { website, host, Social } from './commands';
+
+import { about, Bots, host, Social, Trainging, website } from './commands';
+import { greeting, servicing } from './text';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { development, production } from './core';
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const ENVIRONMENT = process.env.NODE_ENV || '';
+
 const bot = new Telegraf(BOT_TOKEN);
 
-// Register commands
-bot.command('start', (ctx) => {
-  // Handle the start command here
-  // You can provide a welcome message or redirect to the /services command, for example
-  ctx.reply('Welcome to Babyloncenter! Type /services to see available services.');
-});
+bot.command('about', about());
+bot.command('Services', servicing());
+bot.command('Website', website());
+bot.command('Host', host());
+bot.command('Social', Social());
+bot.command('Training', Trainging());
+bot.command('Telegram',Bots())
+bot.on('message', greeting());
 
-bot.command('website', website());
-bot.command('host', host());
-bot.command('social', Social());
 
-// Register text command
-bot.command('services', servicing());
 
-// Register button click handler
-bot.action(/.*/, handleButtonClick());
 
-bot.launch();
+
+//prod mode (Vercel)
+export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
+  await production(req, res, bot);
+};
+//dev mode
+ENVIRONMENT !== 'production' && development(bot);
--- src/text/servicing.ts
+++ src/text/servicing.ts
@@ -1,34 +1,34 @@
-// import { Context } from 'telegraf';
-// import createDebug from 'debug';
+import { Context } from 'telegraf';
+import createDebug from 'debug';
 
-// const debug = createDebug('bot:greeting_text');
+const debug = createDebug('bot:greeting_text');
 
-// const replyToMessage = (ctx: Context, messageId: number, string: string) =>
-//   ctx.reply(string, {
-//     reply_to_message_id: messageId,
-//   });
+const replyToMessage = (ctx: Context, messageId: number, string: string) =>
+  ctx.reply(string, {
+    reply_to_message_id: messageId,
+  });
 
-// const servicing = () => async (ctx: Context) => {
-//   debug('Triggered "service" text command');
+const servicing = () => async (ctx: Context) => {
+  debug('Triggered "service" text command');
 
-//   const messageId = ctx.message?.message_id;
-//   const userName = `${ctx.message?.from.first_name}`;
+  const messageId = ctx.message?.message_id;
+  const userName = `${ctx.message?.from.first_name}`;
 
-//   if (messageId) {
-//     await replyToMessage(ctx, messageId, `
-//     يقدم مركز بابلون مجموعة من الخدمات التعليمية والتكنلوجية المتنوعةابرزها
-//     /Website creation. انشاء مواقع الكترونية
-//     /Host servises. خدمات الاستضافة
-//     /Social Marketing. التسويق الالكتروني
-//     /Training courses. الدورات التدريبية
-//     /Telegram Bots. روبوتات تيليغرام
+  if (messageId) {
+    await replyToMessage(ctx, messageId, `
+    يقدم مركز بابلون مجموعة من الخدمات التعليمية والتكنلوجية المتنوعةابرزها
+    /Website creation. انشاء مواقع الكترونية
+    /Host servises. خدمات الاستضافة
+    /Social Marketing. التسويق الالكتروني
+    /Training courses. الدورات التدريبية
+    /Telegram Bots. روبوتات تيليغرام
     
 
-//   ` );
-//   }
-// };
+  ` );
+  }
+};
 
-// export { servicing };
+export { servicing };
 
 // import { Context, Markup } from 'telegraf';
 // import createDebug from 'debug';
@@ -66,136 +66,4 @@
 
 // export { servicing };
 
-// import { Context, Markup } from 'telegraf';
-// import createDebug from 'debug';
 
-// const debug = createDebug('bot:greeting_text');
-
-// const replyToMessage = (ctx: Context, messageId: number, text: string) => {
-//   ctx.reply(text, {
-//     reply_to_message_id: messageId,
-//   });
-// };
-
-// const servicing = () => async (ctx: Context) => {
-//   const messageId = ctx.message?.message_id;
-
-//   if (messageId) {
-//     // You can use replyToMessage if needed
-//     replyToMessage(ctx, messageId, `
-//       يقدم مركز بابلون مجموعة من الخدمات التعليمية والتكنلوجية المتنوعة ابرزها
-//     `);
-
-//     // Use inline keyboard for services
-//     await ctx.reply('Choose a service:', {
-//       ...Markup.inlineKeyboard([
-//         Markup.button.callback('/Website Creation', '/website'),
-//         Markup.button.callback('/Host Services', 'hosting'),
-//         Markup.button.callback('/Social Marketing', 'instagram'),
-//         Markup.button.callback('Training Courses', 'web'),
-//         Markup.button.callback('Telegram Bots', 'telegram'),
-//       ]),
-//     });
-//   }
-// };
-
-// // Handle button clicks
-// const handleButtonClick = () => async (ctx: Context) => {
-//   const command =(ctx.callbackQuery as any).data;
-
-//   if (command) {
-//     switch (command) {
-//       case '/website':
-//         // Handle Website button click
-//         await ctx.reply('You clicked the Website Creation button!');
-//         break;
-//       case 'hosting':
-//         // Handle Host Services button click
-//         await ctx.reply('You clicked the Host Services button!');
-//         break;
-//       case 'instagram':
-//         // Handle Social Marketing button click
-//         await ctx.reply('You clicked the Social Marketing button!');
-//         break;
-//       case 'web':
-//         // Handle Training Courses button click
-//         await ctx.reply('You clicked the Training Courses button!');
-//         break;
-//       case 'telegram':
-//         // Handle Telegram Bots button click
-//         await ctx.reply('You clicked the Telegram Bots button!');
-//         break;
-//       default:
-//         break;
-//     }
-//   }
-// };
-
-// export { servicing, handleButtonClick };
-
-import { Context, Markup } from 'telegraf';
-import createDebug from 'debug';
-import { website, host, Social } from '../commands';
-const debug = createDebug('bot:greeting_text');
-
-const replyToMessage = (ctx: Context, messageId: number, text: string) => {
-  ctx.reply(text, {
-    reply_to_message_id: messageId,
-  });
-};
-
-const handleButtonClick = () => async (ctx: Context) => {
-  const callbackQuery = ctx.callbackQuery;
-
-  if (callbackQuery) {
-    const command = (ctx.callbackQuery as any).data;
-    console.log('Callback Query Data:', command);
-
-    if (command) {
-      // Handle button clicks based on the command
-      switch (command) {
-        case 'website':
-          // Handle the 'website' command
-          await ctx.answerCbQuery(); // Acknowledge the button click
-          await website()(ctx);
-          break;
-        case 'host':
-          // Handle the 'host' command
-          await ctx.answerCbQuery(); // Acknowledge the button click
-          await host()(ctx);
-          break;
-        case 'social':
-          // Handle the 'social' command
-          await ctx.answerCbQuery(); // Acknowledge the button click
-          await Social()(ctx);
-          break;
-        // Add cases for other commands
-        default:
-          break;
-      }
-    }
-  }
-};
-
-const servicing = () => async (ctx: Context) => {
-  debug('Triggered "service" text command');
-
-  const messageId = ctx.message?.message_id;
-
-  if (messageId) {
-    await replyToMessage(ctx, messageId, `
-      يقدم مركز بابلون مجموعة من الخدمات التعليمية والتكنلوجية المتنوعة ابرزها
-    `);
-
-    // Use inline keyboard for services
-    await ctx.reply('Choose a service:', {
-      ...Markup.inlineKeyboard([
-        Markup.button.callback('Website Creation', 'website'),
-        Markup.button.callback('Host Services', 'host'),
-        Markup.button.callback('Social Marketing', 'social'),
-      ]),
-    });
-  }
-};
-
-export { servicing, handleButtonClick };
