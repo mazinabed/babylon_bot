d7ed9ece526144f6b7bdc79fe0ecc02e5cd18784
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-16 18:57:36 +0300

    adding back commands

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,13 @@ bot.command('instagram', handleButtonClick());
 bot.command('web', handleButtonClick());
 bot.command('telegram', handleButtonClick());
 
-// bot.command('about', about());
+bot.command('about', about());
 bot.command('Services', servicing());
-// bot.command('Website', website());
-// bot.command('Host', host());
-// bot.command('Social', Social());
-// bot.command('Training', Trainging());
-// bot.command('Telegram',Bots())
+bot.command('Website', website());
+bot.command('Host', host());
+bot.command('Social', Social());
+bot.command('Training', Trainging());
+bot.command('Telegram',Bots())
 bot.on('message', greeting());
 
 
